7ee18bc3a6bfdf8773e9f728176c44e112868bdb
Utkarsh Singh <singh.utkarsh95@gmail.com>
2024-02-26 19:39:42 +0530

    updated config.py with online Llama2 download

--- config.py
+++ config.py
@@ -8,7 +8,13 @@ ROOT = FILE.parents[0]
 
 
 hf_token = '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'
-summerizer_model_id = os.path.join(ROOT, "Llama-2-7b-chat-hf")
+# Uncomment the below line if you have locally downloaded Llama-2-7b-chat-hf LLM model.
+#summerizer_model_id = os.path.join(ROOT, "Llama-2-7b-chat-hf")
+
+# Comment this line if you have uncommented line 12 and using local Llama-2-7b-chat-hf LLM model.
+# Downloads model from web and stores it in your local system cache.
+
+summerizer_model_id = "meta-llama/Llama-2-7b-chat-hf"
 store_dir_path = os.path.join(ROOT, "chroma_db" )
 TEXT_SPLITTER_CHUNK = 1000
 TEXT_CHUNK_OVERLAP = 50
